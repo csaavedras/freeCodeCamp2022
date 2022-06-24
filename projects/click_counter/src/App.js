6ef7a39e51b3e9514435db90436a8c30c4edf99a
cisaa <ci.saavedras@gmail.com>
2022-06-23 23:38:52 -0400

    Add: setup main files

---
+++ projects/click_counter/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <main className="App">
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
