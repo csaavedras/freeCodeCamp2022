0f0b7924bc0507ff9d0f1d0757c9be6dc27a5e57
cisaa <ci.saavedras@gmail.com>
2022-06-26 21:22:14 -0400

    Setup basic file with components

---
+++ projects/calculator/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Display from './components/Display';
+import Controls from './components/Controls';
+
+function App() {
+  return (
+    <div className="container">
+      <Display />
+      <Controls />
+    </div>
+  );
+}
+
+export default App;
